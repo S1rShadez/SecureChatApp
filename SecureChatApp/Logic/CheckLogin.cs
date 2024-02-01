1411712aa3eedbf45fa7df5ed7e502ca586391b6
S1rShadez <arthure97@hotmail.com>
2024-02-01 11:42:10 +0100

    Started working on the user auth

---
+++ SecureChatApp/Logic/CheckLogin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureChatApp.Logic
+{
+    public static class CheckLogin
+    {
+        public static bool CLogin(string UName, string PWord)
+        {
+            if (string.IsNullOrEmpty(UName) || string.IsNullOrEmpty(PWord)) { return false; }
+            if(!string.IsNullOrEmpty(UName) ||  !string.IsNullOrEmpty(PWord))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
