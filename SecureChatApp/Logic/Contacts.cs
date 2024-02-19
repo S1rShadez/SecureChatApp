88b8b2f317d236f38ba7a2b33389d62539b3a357
S1rShadez <arthure97@hotmail.com>
2024-02-19 14:52:39 +0100

    added TODO

--- SecureChatApp/Logic/Contacts.cs
+++ SecureChatApp/Logic/Contacts.cs
@@ -9,6 +9,9 @@ namespace SecureChatApp.Logic
     internal class Contacts
     {
         string ID;
+        string IP;
+        //TODO: Update the Contacts class:
+        //msgs should contain datetime of sending/receiving, ID of the contact and a "me" for the user, msg ID based on the datetime to get correct sorting
         List<string> msgs;
     }
 }
